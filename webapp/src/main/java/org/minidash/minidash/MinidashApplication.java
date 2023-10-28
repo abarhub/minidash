e91b778c5f5d025500232dc57abeb6871cf7d08a
abarhub <abarhub@gmail.com>
2023-10-28 15:02:40 +0200

    ajout du module webapp

---
+++ webapp/src/main/java/org/minidash/minidash/MinidashApplication.java
@@ -0,0 +1,13 @@
+package org.minidash.minidash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinidashApplication.class, args);
+	}
+
+}
