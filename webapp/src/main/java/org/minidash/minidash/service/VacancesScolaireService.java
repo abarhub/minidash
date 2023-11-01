bfdafb665a6f6fb97b57fa61e4dbdd27b7a5f683
abarhub <abarhub@gmail.com>
2023-11-01 10:51:43 +0100

    correction de l'affichage des vacances

--- webapp/src/main/java/org/minidash/minidash/service/VacancesScolaireService.java
+++ webapp/src/main/java/org/minidash/minidash/service/VacancesScolaireService.java
@@ -33,71 +33,83 @@ public class VacancesScolaireService {
     @PostConstruct
     public void init() {
         LOGGER.atInfo().log("init vacances");
-        RestTemplate restTemplate = new RestTemplate();
-        String url = urlVacancesScolaires;
-        LocalDate dateDebut = LocalDate.of(2023, Month.JANUARY, 1);
-        LocalDate dateFin = LocalDate.of(2025, Month.DECEMBER, 31);
-        int limite = 100;
-        url += "?where=start_date>=\"" + dateDebut + "\" and end_date<=\"" + dateFin + "\" and zones in (\"Zone A\",\"Zone B\",\"Zone C\")&limit=" + limite ;
-        LOGGER.atInfo().log("url={}", url);
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        if (response.getStatusCode().is2xxSuccessful()) {
-            try {
-                List<VacancesDto> liste = new ArrayList<>();
-                String body=response.getBody();
-                LOGGER.atInfo().log("response: {}",body);
-                ObjectMapper mapper = new ObjectMapper();
-                var res = mapper.readTree(body);
-                if (res != null) {
-                    if (res.has("results")) {
-                        var res2 = res.get("results");
-                        if (res2.isArray() && !res2.isEmpty()) {
-                            for (int i = 0; i < res2.size(); i++) {
-                                var res3 = res2.get(i);
-                                VacancesDto vacancesDto = new VacancesDto();
-                                if (res3.has("description")) {
-                                    vacancesDto.setDescription(res3.get("description").asText());
-                                }
-                                if (res3.has("start_date")) {
-                                    vacancesDto.setDateDebut(convertToDate(res3.get("start_date").asText()));
-                                }
-                                if (res3.has("end_date")) {
-                                    vacancesDto.setDateFin(convertToDate(res3.get("end_date").asText()));
-                                }
-                                if (res3.has("zones")) {
-                                    vacancesDto.setZone(res3.get("zones").asText());
-                                }
-                                if (res3.has("annee_scolaire")) {
-                                    vacancesDto.setAnneeScolaire(res3.get("annee_scolaire").asText());
-                                }
-                                if (vacancesDto.getDateDebut() != null && vacancesDto.getDateFin() != null) {
-                                    liste.add(vacancesDto);
+        List<VacancesDto> listeTotal = new ArrayList<>();
+        for(int annee=2017;annee<2027;annee+=2) {
+            LOGGER.atInfo().log("vacances annee {}-{}",annee,annee+1);
+            RestTemplate restTemplate = new RestTemplate();
+            String url = urlVacancesScolaires;
+            LocalDate dateDebut = LocalDate.of(annee, Month.JANUARY, 1);
+            LocalDate dateFin = LocalDate.of(annee+1, Month.DECEMBER, 31);
+            int limite = 100;
+            url += "?where=start_date>=\"" + dateDebut + "\" and end_date<=\"" + dateFin + "\" and zones in (\"Zone A\",\"Zone B\",\"Zone C\")&limit=" + limite;
+            LOGGER.atInfo().log("url={}", url);
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                try {
+                    List<VacancesDto> liste = new ArrayList<>();
+                    String body = response.getBody();
+                    LOGGER.atInfo().log("response: {}", body);
+                    ObjectMapper mapper = new ObjectMapper();
+                    var res = mapper.readTree(body);
+                    if (res != null) {
+                        if (res.has("results")) {
+                            var res2 = res.get("results");
+                            if (res2.isArray() && !res2.isEmpty()) {
+                                for (int i = 0; i < res2.size(); i++) {
+                                    var res3 = res2.get(i);
+                                    VacancesDto vacancesDto = new VacancesDto();
+                                    if (res3.has("description")) {
+                                        vacancesDto.setDescription(res3.get("description").asText());
+                                    }
+                                    if (res3.has("start_date")) {
+                                        vacancesDto.setDateDebut(convertToDate(res3.get("start_date").asText()));
+                                    }
+                                    if (res3.has("end_date")) {
+                                        vacancesDto.setDateFin(convertToDate(res3.get("end_date").asText()));
+                                    }
+                                    if (res3.has("zones")) {
+                                        vacancesDto.setZone(res3.get("zones").asText());
+                                    }
+                                    if (res3.has("annee_scolaire")) {
+                                        vacancesDto.setAnneeScolaire(res3.get("annee_scolaire").asText());
+                                    }
+                                    if (vacancesDto.getDateDebut() != null && vacancesDto.getDateFin() != null) {
+                                        liste.add(vacancesDto);
+                                    }
                                 }
                             }
                         }
                     }
+                    LOGGER.atInfo().log("nb={}", liste.size());
+                    liste = nettoyage(liste);
+                    LOGGER.atInfo().log("nb vacances {}={}", annee,liste.size());
+                    listeTotal.addAll(liste);
+                } catch (Exception e) {
+                    LOGGER.atError().log("Erreur pour parser le résultat", e);
                 }
-                LOGGER.atInfo().log("nb={}", liste.size());
-                LOGGER.atDebug().log("liste={}", liste);
-                // suppression des doublons
-                Map<String, List<VacancesDto>> result = liste.stream()
-                        .collect(Collectors.groupingBy(map -> map.getDateDebut().toString() + "_" + map.getZone(),
-                                Collectors.mapping(map -> map,
-                                        Collectors.toList())));
-                liste = result.entrySet().stream().map(x -> x.getValue().get(0)).collect(Collectors.toList());
-                LOGGER.atDebug().log("liste2={}", liste);
-                // trie
-                liste.sort(Comparator.comparing(VacancesDto::getDateDebut)
-                        .thenComparing(VacancesDto::getZone));
-                LOGGER.atDebug().log("liste3={}", liste);
-                LOGGER.atInfo().log("nb vacances={}", liste.size());
-                this.listeVacances = liste;
-            } catch (Exception e) {
-                LOGGER.atError().log("Erreur pour parser le résultat", e);
+            } else {
+                LOGGER.atError().log("Erreur pour récupérer les vacances: {}", response.getStatusCode());
             }
-        } else {
-            LOGGER.atError().log("Erreur pour récupérer les vacances: {}", response.getStatusCode());
         }
+        listeTotal = nettoyage(listeTotal);
+        LOGGER.atInfo().log("nb vacances={}", listeTotal.size());
+        this.listeVacances = List.copyOf(listeTotal);
+    }
+
+    private static List<VacancesDto> nettoyage(List<VacancesDto> liste) {
+        LOGGER.atDebug().log("liste={}", liste);
+        // suppression des doublons
+        Map<String, List<VacancesDto>> result = liste.stream()
+                .collect(Collectors.groupingBy(map -> map.getDateDebut().toString() + "_" + map.getZone(),
+                        Collectors.mapping(map -> map,
+                                Collectors.toList())));
+        liste = result.entrySet().stream().map(x -> x.getValue().get(0)).collect(Collectors.toList());
+        LOGGER.atDebug().log("liste2={}", liste);
+        // trie
+        liste.sort(Comparator.comparing(VacancesDto::getDateDebut)
+                .thenComparing(VacancesDto::getZone));
+        LOGGER.atDebug().log("liste3={}", liste);
+        return liste;
     }
 
     public List<VacancesDto> getVacances() {
