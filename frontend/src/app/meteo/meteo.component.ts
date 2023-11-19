b4ee8444ef0327ffd1ecb7a8fd393963cea76535
abarhub <abarhub@gmail.com>
2023-11-19 18:02:17 +0100

    nettoyage du code de la météo

--- frontend/src/app/meteo/meteo.component.ts
+++ frontend/src/app/meteo/meteo.component.ts
@@ -3,9 +3,6 @@ import {HttpClient} from "@angular/common/http";
 import {MeteoModel} from "../model/meteo.model";
 import {ChartConfiguration, ChartOptions} from "chart.js";
 import {DatePipe} from "@angular/common";
-// import {Chart, ChartType, registerables} from 'chart.js';
-
-// Chart.register(...registerables);
 
 
 @Component({
@@ -17,10 +14,6 @@ import {DatePipe} from "@angular/common";
 export class MeteoComponent implements OnInit {
 
   public meteo: MeteoModel | null = null;
-
-  // public myChart: Chart<ChartType, number[], string>|null=null;
-
-  // public chart: any;
 
 
 
@@ -34,44 +27,9 @@ export class MeteoComponent implements OnInit {
 
       this.calculPrecipitation(data);
 
-      // this.myChart = new Chart("myChart", {
-      //   type: 'bar',
-      //   data: {
-      //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      //     datasets: [{
-      //       label: '# of Votes',
-      //       data: [12, 19, 3, 5, 2, 3],
-      //       backgroundColor: [
-      //         'rgba(255, 99, 132, 0.2)',
-      //         'rgba(54, 162, 235, 0.2)',
-      //         'rgba(255, 206, 86, 0.2)',
-      //         'rgba(75, 192, 192, 0.2)',
-      //         'rgba(153, 102, 255, 0.2)',
-      //         'rgba(255, 159, 64, 0.2)'
-      //       ],
-      //       borderColor: [
-      //         'rgba(255, 99, 132, 1)',
-      //         'rgba(54, 162, 235, 1)',
-      //         'rgba(255, 206, 86, 1)',
-      //         'rgba(75, 192, 192, 1)',
-      //         'rgba(153, 102, 255, 1)',
-      //         'rgba(255, 159, 64, 1)'
-      //       ],
-      //       borderWidth: 1
-      //     }]
-      //   },
-      //   options: {
-      //     scales: {
-      //       y: {
-      //         beginAtZero: true
-      //       }
-      //     }
-      //   }
-      // });
 
     });
 
-    // this.createChart();
   }
 
   recharger() {
@@ -81,35 +39,6 @@ export class MeteoComponent implements OnInit {
     });
   }
 
-  // createChart(){
-  //
-  //   this.chart = new Chart("MyChart", {
-  //     type: 'bar', //this denotes tha type of chart
-  //
-  //     data: {// values on X-Axis
-  //       labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
-  //         '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17', ],
-  //       datasets: [
-  //         {
-  //           label: "Sales",
-  //           data: ['467','576', '572', '79', '92',
-  //             '574', '573', '576'],
-  //           backgroundColor: 'blue'
-  //         },
-  //         {
-  //           label: "Profit",
-  //           data: ['542', '542', '536', '327', '17',
-  //             '0.00', '538', '541'],
-  //           backgroundColor: 'limegreen'
-  //         }
-  //       ]
-  //     },
-  //     options: {
-  //       aspectRatio:2.5
-  //     }
-  //
-  //   });
-  // }
 
   public lineChartData: ChartConfiguration<'line'>['data'] = {
     labels: [
