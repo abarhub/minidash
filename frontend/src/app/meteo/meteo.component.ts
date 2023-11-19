25815b473f55b61c290734e568f231fa5bcd89dd
abarhub <abarhub@gmail.com>
2023-11-19 18:16:54 +0100

    ajout de la température des prochaines heures

--- frontend/src/app/meteo/meteo.component.ts
+++ frontend/src/app/meteo/meteo.component.ts
@@ -27,6 +27,8 @@ export class MeteoComponent implements OnInit {
 
       this.calculPrecipitation(data);
 
+      this.calculTemperatire(data);
+
 
     });
 
@@ -66,6 +68,28 @@ export class MeteoComponent implements OnInit {
   };
   public lineChartLegend = true;
 
+  public lineChartData2: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July'
+    ],
+    datasets: [
+      {
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        label: 'Précipitations',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)'
+      }
+    ]
+  };
+
   private calculPrecipitation(data: MeteoModel) {
     if(data&&data.precipitations){
       let labels:string[]=[];
@@ -73,7 +97,7 @@ export class MeteoComponent implements OnInit {
       for(let i=0;i<data.precipitations.length;i++){
         let tmp=data.precipitations[i];
         if(tmp&&tmp.date&&tmp.precipitation>=0) {
-          let heure=this.datePipe.transform(tmp.date, 'hh:mm');
+          let heure=this.datePipe.transform(tmp.date, 'HH:mm');
           if(heure) {
             labels.push(heure);
             datas.push(tmp.precipitation);
@@ -98,4 +122,41 @@ export class MeteoComponent implements OnInit {
     }
 
   }
+
+  private calculTemperatire(data: MeteoModel) {
+    if(data&&data.prochainesHeures){
+      let labels:string[]=[];
+      let datas:number[]=[];
+      for(let i=0;i<data.prochainesHeures.length;i++){
+        let tmp=data.prochainesHeures[i];
+        if(tmp&&tmp.date) {
+          let heure=this.datePipe.transform(tmp.date, 'HH:mm');
+          if(heure) {
+            labels.push(heure);
+            datas.push(tmp.temperature);
+          }
+        }
+      }
+      if(labels.length>0&&labels.length==datas.length){
+        let label='Température';
+        if(data&&data.prochainesHeures&&data.prochainesHeures.length>0){
+          label='Température '+data.prochainesHeures[0].date;
+        }
+        this.lineChartData2 = {
+          labels: labels,
+          datasets: [
+            {
+              data: datas,
+              label: label,
+              fill: true,
+              tension: 0.5,
+              borderColor: 'black',
+              backgroundColor: 'rgba(255,0,0,0.3)'
+            }
+          ]
+        };
+      }
+    }
+  }
+
 }
