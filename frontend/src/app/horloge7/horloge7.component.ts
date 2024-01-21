1940910078409f945e3fdeafbd62fb3827efdc6d
abarhub <abarhub@gmail.com>
2024-01-21 09:30:42 +0100

    amélioration de l'horloge 7 avec des signaux

--- frontend/src/app/horloge7/horloge7.component.ts
+++ frontend/src/app/horloge7/horloge7.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 @Component({
@@ -11,28 +11,43 @@ import {CommonModule} from '@angular/common';
 export class Horloge7Component implements OnInit//, AfterViewInit
 {
 
-  secondsDegrees:number=0;
-  minsDegrees:number=0;
-  hourDegrees:number=0;
+  secondsDegrees: number = 0;
+  minsDegrees: number = 0;
+  hourDegrees: number = 0;
+  secondsDegreesAsync = signal(0);
+  minsDegreesAsync = signal(0);
+  hourDegreesAsync = signal(0);
 
   ngOnInit() {
     setInterval(() => {
       this.setDate();
-    }, 500);
+    }, 1000);
 
   }
 
   private setDate() {
     const now = new Date();
 
+    const lastSecondsDegrees = this.secondsDegrees;
     const seconds = now.getSeconds();
-    this.secondsDegrees=((seconds / 60) * 360) + 90;
+    this.secondsDegrees = ((seconds / 60) * 360) + 90;
 
+    const lastMinsDegrees = this.minsDegrees;
     const mins = now.getMinutes();
-    this.minsDegrees=((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    this.minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
 
+    const lastHourDegrees = this.hourDegrees;
     const hour = now.getHours();
-    this.hourDegrees=((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    this.hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    if (lastSecondsDegrees != this.secondsDegrees) {
+      this.secondsDegreesAsync.set(this.secondsDegrees);
+    }
+    if (lastMinsDegrees != this.minsDegrees) {
+      this.minsDegreesAsync.set(this.minsDegrees);
+    }
+    if (lastHourDegrees != this.hourDegrees) {
+      this.hourDegreesAsync.set(this.hourDegrees);
+    }
   }
 
 
